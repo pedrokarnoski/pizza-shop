ac4efe0bcccf8c0b4427807e502f2ce2ad1f50d3
Pedro Henrique Karnoski <pedrokarnoski@gmail.com>
2024-03-22 21:26:46 -0300

    fix: Spelling error

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -7,7 +7,7 @@ export function NotFound() {
       <h1 className="text-8xl font-extrabold tracking-widest">404</h1>
       <ShieldX className="h-16 w-16" />
       <h2 className="text-3xl font-bold">Página não encontrada</h2>
-      <p className="text-accent-foregound">
+      <p className="text-accent-foreground">
         Voltar para o{' '}
         <Link to="/" className="font-medium text-sky-500 dark:text-sky-400">
           Dashboard
