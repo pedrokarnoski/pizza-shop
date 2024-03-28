1ad78e1023554014f17a611044ef121c10b3015a
Pedro Henrique Karnoski <pedrokarnoski@gmail.com>
2024-03-27 22:59:37 -0300

    feat: Error page

---
+++ src/pages/error.tsx
@@ -0,0 +1,23 @@
+import { RefreshCwOff } from 'lucide-react'
+import { Link, useRouteError } from 'react-router-dom'
+
+export function Error() {
+  const error = useRouteError() as Error
+
+  return (
+    <div className="flex h-screen flex-col items-center justify-center gap-2 space-y-4">
+      <RefreshCwOff className="h-16 w-16" />
+      <h2 className="text-3xl font-bold">Whoops, algo aconteceu...</h2>
+      <p className="text-accent-foreground">
+        Um erro aconteceu na aplicação, abaixo você encontra mais detalhes:
+      </p>
+      <pre>{error?.message || JSON.stringify(error)}</pre>
+      <p className="text-accent-foreground">
+        Voltar para o{' '}
+        <Link to="/" className="font-medium text-sky-500 dark:text-sky-400">
+          Dashboard
+        </Link>
+      </p>
+    </div>
+  )
+}
